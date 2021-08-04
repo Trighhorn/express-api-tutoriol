62a05078ec758a67eab975900861519aaf9cda19
trighhorn <trighhorn@gmail.com>
2021-08-04 15:01:53 -0600

    added a post request

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const PORT = 8080
 
 app.listen(
     PORT,
-    () => console.log('Hello from localhost:' + PORT)
+    () => console.log(`Hello from localhost:${PORT}`)
 )
 
 app.get('/tshirt', (req, res) => {
     res.status(200).send({
-        tshirt: '1',
         size: 'large'
     })
 })
 
+app.post('/tshirt/:id', (req, res) =>{
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'Logo Required'})
+    }
+    res.send({
+        tshirt: `Tshirt with your ${logo} and ID of ${id}` 
+    })
+})
